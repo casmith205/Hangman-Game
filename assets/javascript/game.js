17359787794d8d78f191ae600810e89a105e5abe
Carolyn Smith <casmith205@gmail.com>
2018-03-07 16:58:07 -0600

    added in-order array replacement for correct letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,105 @@
-// Creating a function to determine if user correctly guessed
-// Will need to adjust this function (or create another) where the computer guess is a word not a letter and match a guess to the array
-function isPsychic (user, computer){
+// DECLARING FUNCTIONS
+    // Start the game by entering in the blank letter spaces
+    function start (arrFrom, arrTo){
+        blank = arrFrom.length
+        for(i=0; i<blank; i++){
+            arrTo[i] = "_";
+        }
+    }
+    // Determine if an input (user) is in an array (arr)
+    function inArray(user, arr){
+        var count=arr.length;
+        for(var i=0;i<count;i++){
+            if(arr[i]===user){return true;}
+        }
+        return false;
+    };
 
-    if(user===computer){
-        return "Win"
-    } else {
-        return "Incorrect"
+    // Reset the game after a win/loss
+    function reset (){
+        guessesLeft=10;
+        guessedLetters=[];
+        computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerChar = computerWord.split("");
+    };
+
+    // Count the number of times a character is in an array
+    function countInArray(arr){
+        var count=arr.length;
+        for (i=0, j=0; count>i; i++, j++){
+            if(arr[i]==arr[j]){
+                return "repeat"
+            };
+         };
+    };
+
+    function correctArrayReplacement(arrKey, arrCorrect, guess){
+        for(i=0; i<=arrKey.length; i++){
+            if(arrKey[i]==guess){
+            arrCorrect.splice(i, 0, guess)
+            };
+        }
+        document.getElementById("current").innerHTML = arrCorrect;
     }
 
+// DEFINING VARIABLES 
+    // Connect variables to HTML & starting position
+    var wins = document.getElementById("wins");
+    var losses = document.getElementById("losses");
+    var guessesLeft = 9; 
+    var guessedLetters = [];
+    var correctLetters = [];
+
+
+
+// ARRAYS
+    // Creates an array for the computer choices - array of different words
+    computerChoices = ["brockhampton", "drake", "amine", "migos","dram","russ","future","kendrick","kanye"];
+
+    // Cretes an array for the user vailable options (entire alphabet - lowercase)
+    userChoices = [];
+    for (i=97;i<=122;i++){
+        userChoices[userChoices.length] = String.fromCharCode(i);
+    };
+
+    // Get computer's word
+    var computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    // Turn computer's word into characters
+    var computerChar = computerWord.split("");
+    console.log(computerChar);
+
+document.onload = function (){
+    start(computerChar, correctLetters);
 };
 
-// Function to determine if an input (user) is in an array (arr)
-function inArray(user, arr)
-{
-    var count=arr.length;
-    for(var i=0;i<count;i++)
-    {
-        if(arr[i]===user){return true;}
-    }
-    return false;
-}
-
-
-
-// Reset the funtion after a win/loss
-function reset (){
-    guessesLeft=10;
-    guessedLetters=[];
-    computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
-
-// Creates an array for the computer choices - array of different words
-computerChoices = ["brockhampton", "drake", "amine", "migos","dram","russ","future","kendrick"];
-
-// Cretes an array for the user vailable options (entire alphabet - lowercase)
-userChoices = [];
-for (i=97;i<=122;i++){
-    userChoices[userChoices.length] = String.fromCharCode(i);
-};
-
-
-
-// Connect variables to HTML & starting position
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
-var guessesLeft = 9; 
-var guessedLetters = [];
-
-
-// Get computer's guess
-var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-// This function is run whenever the user presses a key.
+// USER PRESSES A KEY...
 document.onkeyup = function (event) {
     // Checking what the computer generated for testing purposes
-    console.log(computerLetter);
+    console.log(computerWord);
     
     // Determines which key was pressed.
     var userGuess = event.key;
     console.log(userGuess);
     
-    // Checks if input is accpetable
+    // Checks if input is accpetable (a letter)
     var validLetter = inArray(userGuess, userChoices); 
     console.log(validLetter);
 
-    // creating a "result" variable for the resilt of the isPsychic function
-    var result = isPsychic(userGuess, computerLetter);
-    console.log(result);
+    // Creates a variable for if the quessed letter is in the current string
+    var correctLetterCheck = inArray(userGuess,computerChar);
+    console.log(correctLetterCheck);
 
-    // Creating an "If" stmt to first check if valids input, and then if win/incorrect result
+    // Checking for repeat letters
+    var repeatLetters = countInArray(computerChar);
+    console.log(repeatLetters);
+
+    // Creating an "If" stmt to first check if valids input (abc element)
     if(validLetter == true){
-        // Creating an "If" stmt to tell what to do for a win/incorrect result
-        if( result=="Win"){
-            // add a win into the log
-            wins++;
-            reset ();
-            
+        // Creating an "If" stmt to check if the letter is in the computer word string
+        if( correctLetterCheck==true){
+            //Calls function to add in the correct location
+            correctArrayReplacement(computerChar, correctLetters, userGuess);
         } else {
             // reduce the guesses left 
             guessesLeft--;
@@ -87,19 +111,35 @@ document.onkeyup = function (event) {
             // add a loss into the log
             losses++;
             reset ()
+        }
+        
+        if (repeatLetters="Repeat"){
+            if(correctLetters.length == (computerChar.length)-1) {
+                // add a win into the log
+                wins++;
+                reset ();
+            }
+        } else if(correctLetters.length == computerChar.length) {
+                // add a win into the log
+                wins++;
+                reset ();
+            }
 
-        } 
+
         
     } else {
             alert("Invalid input! Please use the alphabet :)")
 
     };  
 
+
+
     // Changing HTML text
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("losses").innerHTML = losses;
     document.getElementById("remainingGuesses").innerHTML = guessesLeft;
     document.getElementById("guessedLetters").innerHTML = guessedLetters;
+    document.getElementById("current").innerHTML = correctLetters;
 
 
 console.log(wins);
